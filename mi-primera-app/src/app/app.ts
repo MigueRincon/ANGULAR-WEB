9ed4f1d805131e891e55eb710355c94e97454a29
MigueRincon <rinrincon527@gmail.com>
2026-08-14 14:11:59 +0000

    oki

--- mi-primera-app/src/app/app.ts
+++ mi-primera-app/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { Saludo } from './components/saludo/saludo';
+import { Usuario } from './components/usuario/usuario';
 
 @Component({
   selector: 'app-root',
-  imports: [Saludo],
+  imports: [Saludo, Usuario],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- mi-primera-app/src/app/components/usuario/usuario.ts
+++ mi-primera-app/src/app/components/usuario/usuario.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-usuario',
-  imports: [],
+  standalone: true,
   templateUrl: './usuario.html',
   styleUrl: './usuario.css',
 })
-export class Usuario {}
+export class Usuario {
+  nombre: string = 'Miguel Rincon';
+  edad: number = 23;
+  activo: boolean = true;
+}
